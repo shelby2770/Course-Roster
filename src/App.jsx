33becbab82dd44cdee64516ddc9e66a95dd6fa23
Adnan Istiaque <108503483+shelby2770@users.noreply.github.com>
2023-09-15 20:55:04 +0600

    changed directory of fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>Hello React!</h1>
+      <h2 className='text-2xl bg-green-600'>Hello World</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
